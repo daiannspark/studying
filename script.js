7c5ee5a6084e948aea73cc90ca0c2a01f3ecc52f
Diana <daiannspark@gmail.com>
2019-05-26 20:10:19 +0300

    loops

--- script.js
+++ script.js
@@ -1,23 +1,70 @@
-let money = prompt ("Ваш бюджет на месяц?", "");
+'use strict';
+
+let money = +prompt ("Ваш бюджет на месяц?", "");
 let time = prompt ("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
     budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
     timeData: time,
-    expenses:{
-        "питание" : "20 евро"
-    },
-    optionalExpenses:{},
-    income:[],
     savings: false
+};
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+
+    if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != '' && b != '' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b; 
+    } else {
+        console.log('ups');
+    }
+};
+
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//     b = prompt("Во сколько обойдется?", '');
+//     i++;
+
+//     if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != '' && b != '' && a.length < 50) {
+//         console.log("done");
+//         appData.expenses[a] = b; 
+//     } else {
+//         console.log('ups');
+//     }
+// };
+
+
+// let i = 0;
+// do {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//     b = prompt("Во сколько обойдется?", '');
+//     i++;
+
+//     if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != '' && b != '' && a.length < 50) {
+//         console.log("done");
+//         appData.expenses[a] = b; 
+//     } else {
+//         console.log('ups');
+//     }
+// } while (i < 2);
+
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert ("Daily budget:" + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("Not so much");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Medium level");
+} else if (appData.moneyPerDay > 2000) {
+    console.log ("Very good");
+} else {
+    console.log("Shit happens");
 }
-
-// let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-// 	a2 = prompt("Во сколько обойдется?", ''),
-// 	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-// 	a4 = prompt("Во сколько обойдется?", '');
-
-// appData.expenses.a1 = a2;
-// appData.expenses.a3 = a4;
-
-alert (appData.budget / 30);
